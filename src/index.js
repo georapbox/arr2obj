9b3e0c483f9b2c2c44abbaf540fa664114ce522a
georapbox <georapbox@gmail.com>
2016-12-07 09:15:01 +0200

    Check if current item is plain object before deleting property

--- src/index.js
+++ src/index.js
@@ -8,26 +8,32 @@ import extend from './util/extend';
  *
  * @param {Array} array The initial array to convert.
  * @param {Object} [options] User defined options to override defaults.
- * @param {String} [options.key] If `array` is an array of objects and each object has a property with the value of `key`, use those as the properties of the result object. By default the index of each object in array is used.
- * @param {String} [options.keyPrefix=''] A string that prefixes the values of the specified `key`. If anything than string is provided, it takes the default value ('').
- * @param {String} [options.keySuffix=''] A string that suffixes the values of the specified `key`. If anything than string is provided, it takes the default value ('').
- * @param {Boolean} [options.deleteKey=false] If `key` is specified and `options.deleteKey` is true, delete the property from the result object.
+ * @param {String} [options.key] If `array` is an array of objects and each object has a property with the value of `key`, use those as the properties of the result object. By default the index of each item in array is used.
+ * @param {String} [options.keyPrefix=''] A string that prefixes the keys of the result object. If anything than string is provided, it takes the default value ('').
+ * @param {String} [options.keySuffix=''] A string that suffixes the keys of the result object. If anything than string is provided, it takes the default value ('').
+ * @param {Boolean} [options.deleteKey=false] Deletes the property of each item of the result object if `key` is specified and exists in current object. If `key` does not exist in current item or current item is not an object, leaves the property as is.
  * @param {Boolean} [options.inheritProto=false] Determines if the result object should inherit from the `Object.prototype`.
  * @throws {TypeError} Will throw if `array` is not an Array.
  * @return {Object} The converted object.
  * @example
  *
- * const arr1 = [
+ * const objectsArray = [
  *   {_id: 'u0', name: 'John Doe', job: 'Frontend Developer'},
  *   {_id: 'u1', name: 'Albert Brown', job: 'Designer'},
  *   {_id: 'u2', name: 'Stuart Chalmers', job: 'Product Manager'},
  *   {_id: 'u3', name: 'Alberta Jonson', job: 'Account Manager'}
  * ];
  *
- * const arr2 = ['orange', 'banana', 'apple', 'cherry'];
+ * const stringsArray = ['orange', 'banana', 'apple', 'cherry'];
+ *
+ * const mixedArray = [
+ *   {_id: 'u0', name: 'John Doe', job: 'Frontend Developer'},
+ *   ['orange', 'banana', 'apple', 'cherry'],
+ *   'Just a string'
+ * ];
  *
  * // Convert an array of objects using the "_id" property as key of the result object, prefixed with "$" character.
- * arr2obj(arr1, {
+ * arr2obj(objectsArray, {
  *   key: '_id',
  *   keyPrefix: '$'
  * });
@@ -39,7 +45,7 @@ import extend from './util/extend';
  * // -> }
  *
  * // Convert an array of objects using the "_id" property as key of the result object, and delete it from the result object.
- * arr2obj(arr1, {
+ * arr2obj(objectsArray, {
  *   key: '_id',
  *   deleteKey: true
  * });
@@ -51,15 +57,25 @@ import extend from './util/extend';
  * // -> }
  *
  * // Convert an array of strings with default options.
- * arr2obj(arr2);
+ * arr2obj(stringsArray);
  * // -> { '0': 'orange', '1': 'banana', '2': 'apple', '3': 'cherry' }
  *
  * // Convert an array of strings prefixing and suffixing the keys with "_" character.
- * arr2obj(arr2, {
+ * arr2obj(stringsArray, {
  *   keyPrefix: '_',
  *   keySuffix: '_'
  * });
  * // -> { '_0_': 'orange', '_1_': 'banana', '_2_': 'apple', '_3_': 'cherry' }
+ *
+ * // Convert an array of mixed types using the "_id" property as key of the result object.
+ * arr2obj(mixedArray, {
+ *   key: '_id'
+ * });
+ * // -> {
+ * // ->   '1': [ 'orange', 'banana', 'apple', 'cherry' ],
+ * // ->   '2': 'Just a string',
+ * // ->   u0: { _id: 'u0', name: 'John Doe', job: 'Frontend Developer' }
+ * // -> }
  */
 function arr2obj(array, options) {
   if (!isArray(array)) {
@@ -105,7 +121,7 @@ function arr2obj(array, options) {
       accum[keyPrefix + current[key] + keySuffix] = current;
     }
 
-    if (key && deleteKey) {
+    if (deleteKey && key && isPlainObject(current)) {
       delete current[key];
     }
 
